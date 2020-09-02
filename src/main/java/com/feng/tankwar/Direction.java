0d17628e99aeebfe2867638cf02ec1745586e6f2
Minfeng Xu <minfeng.xu1119@gmail.com>
2020-09-02 15:22:05 -0400

    tank move 8 direction

---
+++ src/main/java/com/feng/tankwar/Direction.java
@@ -0,0 +1,12 @@
+package com.feng.tankwar;
+
+public enum  Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    UPLEFT,
+    UPRIGHT,
+    DOWNLEFT,
+    DOWNRIGHT
+}
--- src/main/java/com/feng/tankwar/GameClient.java
+++ src/main/java/com/feng/tankwar/GameClient.java
@@ -2,18 +2,23 @@ package com.feng.tankwar;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class GameClient extends JComponent {
 
+    private Tank playerTank;
+
+
+    private GameClient() {
+        this.playerTank = new Tank(400,100, Direction.DOWN);
+        this.setPreferredSize(new Dimension(800, 600));
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(),400, 100, null );
-    }
-
-    private GameClient() {
-        this.setPreferredSize(new Dimension(800, 600));
+        playerTank.draw(g);
     }
 
     public static void main(String[] args) {
@@ -25,7 +30,28 @@ public class GameClient extends JComponent {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setLocationRelativeTo(null);
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                client.playerTank.keyPressed(e);
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                client.playerTank.keyReleased(e);
+            }
+        });
         frame.setVisible(true);
+
+        while (true) {
+            client.repaint();
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
